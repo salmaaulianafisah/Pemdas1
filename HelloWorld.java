443a8fd107c7a0e2fd995627232654d8d44feb6d
Salma Aulia Nafisah <salmaaulnfsh@gmail.com>
2025-09-21 12:37:25 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+// nama: Salma Aulia Nafisah
+// nim: 2511016220009
+// buat komentar satu baris atau menonaktifkan baris
+/*komentar beberapa baris
+ * baris satu
+ * baris dua
+ */
+// class
+public class HelloWorld {
+
+    // main
+    public static void main(String[] args) { //public static nama_method (tipe_data parameter)
+        // kode program
+        System.out.println("Hello World"); //diakhiri titik koma
+        System.out.print("Nama: "); // jika menggunakan print yang selanjutnya akan satu baris
+        System.out.println("Salma"); // jika menggunakan println selanjutnya baris baru
+        System.out.print("NIM: ");
+        System.out.println("2511016220009");
+        System.out.print("Umur: "); //string - kumpulan karakter
+        System.out.println(18); //int
+    }
+}
+/*Nama Class:
+ * - tidak ada spasi
+ * - tidak diawali dengan angka
+ */
